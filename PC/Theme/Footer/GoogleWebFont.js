0e31f7dea138003bf444440b87166509c5d1f564
guitarrapc <cherishacquire@outlook.com>
2015-11-22 05:19:59 +0900

    Title Description の font も修正

--- PC/Theme/Footer/GoogleWebFont.js
+++ PC/Theme/Footer/GoogleWebFont.js
@@ -1,4 +1,5 @@
 <!-- Goolge Web Font for Title -->
 <head>
-<link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' type='text/css'>
 </head>
